627eab12d71d2a672a2cb350a25a6a0e8e34c3bd
yageloshri <yagel557@gmail.com>
2025-05-09 02:16:54 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
-
-# עדכון והתקנת כלים דרושים
-apt-get update && apt-get install -y ffmpeg fluidsynth curl
-
-# התקנת תלויות פייתון
-pip install --no-cache-dir -r requirements.txt
+apt-get update && apt-get install -y ffmpeg
+pip install -r requirements.txt
