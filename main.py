fb14e71f686118377be7dcabfa8a0e5fc3523b5f
yageloshri <yagel557@gmail.com>
2025-05-09 01:23:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import soundfile as sf
 import os
 from pydub import AudioSegment
 from music21 import stream, note, chord, midi
+import subprocess
 
 app = FastAPI()
 
@@ -28,34 +29,3 @@ async def analyze_audio(file: UploadFile = File(...)):
     for i in range(pitches.shape[1]):
         index = magnitudes[:, i].argmax()
         pitch = pitches[index, i]
-        if pitch > 0:
-            melody.append(librosa.hz_to_note(pitch))
-
-    # יצירת הרמוניה בסיסית
-    midi_stream = stream.Stream()
-    for pitch_name in melody[:16]:
-        try:
-            n = note.Note(pitch_name)
-            c = chord.Chord([n, n.transpose(4), n.transpose(7)])
-            midi_stream.append(c)
-        except:
-            continue
-
-    midi_fp = "output.mid"
-    mf = midi.translate.streamToMidiFile(midi_stream)
-    mf.open(midi_fp, 'wb')
-    mf.write()
-    mf.close()
-
-    # המרת MIDI ל-MP3
-    audio = AudioSegment.from_file(midi_fp, format="mid")
-    mp3_fp = "output.mp3"
-    audio.export(mp3_fp, format="mp3")
-
-    # ניקוי קבצים זמניים
-    os.remove(input_path)
-    if converted_path != input_path:
-        os.remove(converted_path)
-    os.remove(midi_fp)
-
-    return FileResponse(mp3_fp, media_type="audio/mp3", filename="harmonized.mp3")
